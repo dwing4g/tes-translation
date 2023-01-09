25865e6f8c75e57821b09eaebfa904029d7a3d10
amazingfate <liujianfeng1994@gmail.com>
2023-01-09 19:19:47 +0800

    use gbk

--- generate.sh
+++ generate.sh
@@ -4,6 +4,6 @@ do
 	txt_file=${txt_file%.txt}
 	echo $txt_file
 	iconv -f gbk -t utf-8 ${txt_file}.txt > ${txt_file}.txt_utf8
-	sed -i 's/\r$//' ${txt_file}.txt_utf8
-	luajit ./tes3enc.lua ${txt_file}.txt_utf8 ${txt_file}.esp
+	sed -i 's/\r$//' ${txt_file}.txt*
+	luajit ./tes3enc.lua ${txt_file}.txt ${txt_file}.esp
 done
