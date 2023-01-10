b571af8925119d3e67bc270af62f448855d82050
jfliu <jfliu@zshield.net>
2023-01-10 14:46:57 +0800

    add utf8 support

--- generate.sh
+++ generate.sh
@@ -3,7 +3,9 @@ for txt_file in $(ls *.txt)
 do
 	txt_file=${txt_file%.txt}
 	echo $txt_file
-	iconv -f gbk -t utf-8 ${txt_file}.txt > ${txt_file}.txt_utf8
-	sed -i 's/\r$//' ${txt_file}.txt*
-	luajit ./tes3enc.lua ${txt_file}.txt ${txt_file}.esp
+	sed -i 's/\r$//' ${txt_file}.txt
+	luajit ./tes3enc.lua ${txt_file}.txt ${txt_file}_gbk.esp
+	luajit ./tes3dec.lua ${txt_file}_gbk.esp gbk > ${txt_file}.txt_gbk
+	iconv -f gbk -t utf-8 ${txt_file}.txt_gbk > ${txt_file}.txt_utf8
+	luajit ./tes3enc.lua ${txt_file}.txt_utf8 ${txt_file}_utf8.esp
 done
