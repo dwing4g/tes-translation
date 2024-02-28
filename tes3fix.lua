65f18cd98dc8c1eb2ce73b89fe67a081bfbb3444
dwing <dwing4g@gmail.com>
2024-02-28 18:37:33 +0800

    fix tes3cn_*.txt

--- tes3fix.lua
+++ tes3fix.lua
@@ -2,7 +2,11 @@
 
 local io = io
 local arg = arg
+local ipairs = ipairs
 local print = print
+local error = error
+local char = string.char
+local concat = table.concat
 
 local function getEnd(line)
 	local ee = ""
@@ -35,6 +39,9 @@ local trans = {
 	["...."] = "¡­¡­",
 	["....."] = "¡­¡­",
 	["......"] = "¡­¡­",
+	["......."] = "¡­¡­¡­",
+	["........"] = "¡­¡­¡­",
+	["........."] = "¡­¡­¡­",
 	["??"] = "£¿£¿",
 	["?!"] = "£¿£¡",
 	["!?"] = "£¡£¿",
@@ -110,6 +117,95 @@ local function fix(e, c)
 	return cc and (c .. " " .. cc) or c
 end
 
+local function fixMark(e, c)
+	if not c:find "[\x80-\xff] +[\x80-\xff]" then
+		return c
+	end
+	local cc = c:match "{[^{}]*}$"
+	if cc then
+		c = c:sub(1, -#cc - 1):gsub("[ \t]+$", "")
+	end
+	local em = {}
+	for s in e:gmatch "[,.;:?!]+" do
+		if #s == 1 then
+			em[#em + 1] = s
+		else
+			for i = 1, #s do
+				local ss = s:sub(i, i)
+				if ss == "." and s:sub(i - 1, i - 1) == "." then
+					em[#em] = em[#em] .. "."
+				else
+					em[#em + 1] = ss
+				end
+			end
+		end
+	end
+	for _, m in ipairs(em) do
+		if not trans[m] then
+			error("ERROR: unknown mark: " .. m)
+		end
+	end
+	local ct = {}
+	local half = false
+	for i = 1, #c do
+		local b = c:byte(i)
+		if half then
+			ct[#ct + 1] = c:sub(i - 1, i)
+			half = false
+		elseif b >= 0x80 then
+			half = true
+		else
+			ct[#ct + 1] = char(b)
+		end
+	end
+	if half then
+		error("ERROR: unexpected half char: " .. c)
+	end
+	local j = 1
+	local fail = false
+	for i, b in ipairs(ct) do
+		if b == "¡£" or b == "£»" or b == "£¿" or b == "£¡" then
+			if trans[em[j]] == b then
+				j = j + 1
+			else
+				fail = true
+				break
+			end
+		elseif b == "£º" then
+			if em[j] == ":" or em[j] == "." then
+				j = j + 1
+			else
+				fail = true
+				break
+			end
+		elseif b == "£¬" then
+			if em[j] == "," or em[j] == "." then
+				j = j + 1
+			else
+				fail = true
+				break
+			end
+		elseif b == "¡­" and ct[i - 1] ~= "¡­" then
+			if em[j] and #em[j] > 1 then
+				j = j + 1
+			else
+				fail = true
+				break
+			end
+		elseif b == " " then
+			ct[i] = trans[em[j]]
+			j = j + 1
+		end
+	end
+	if j ~= #em + 1 then
+		fail = true
+	end
+	if not fail then
+		c = concat(ct)
+	end
+	return cc and (c .. " " .. cc) or c
+end
+
 local lines = {}
 local checks = {}
 local n, s, e = 0, 0
@@ -126,7 +222,7 @@ for line in io.lines(arg[1]) do
 			end
 		else
 			if line:sub(1, 1) ~= '"' then
-				local line1 = fix(e, line)
+				local line1 = fixMark(e, fix(e, line))
 				if line1 ~= line then
 					line = line1
 					n = n + 1
@@ -150,7 +246,7 @@ end
 
 if n > 0 then
 	local f = io.open(arg[1], "wb")
-	f:write(table.concat(lines, "\r\n"), "\r\n")
+	f:write(concat(lines, "\r\n"), "\r\n")
 	f:close()
 end
 print("done! (" .. n .. " fixed)")
