42344156c2ade6bc8c74ab98cf91e6fc403a02be
dwing <dwing4g@gmail.com>
2025-02-05 12:11:33 +0800

    some fixes for tes3cn_*; add tes3forbid.lua

---
+++ tes3forbid.lua
@@ -0,0 +1,38 @@
+local ipairs = ipairs
+
+local filenames = {
+	"tes3cn_Morrowind.ext.txt",
+	"tes3cn_Tribunal.ext.txt",
+	"tes3cn_Bloodmoon.ext.txt",
+}
+
+local forbids = {
+	"艾德拉", -- 伊德拉
+	"艾德洛", -- 伊德拉
+	"代德拉", -- 迪德拉
+	"代德洛", -- 迪德拉
+	"勒多兰", -- 瑞多然
+	"希尔辛", -- 海尔辛
+	"灰烬汉", -- 灰烬可汗
+	"灰烬汗", -- 灰烬可汗
+	"古拉汉", -- 古拉可汗
+	"古拉汗", -- 古拉可汗
+	"马拉卡斯", -- 玛拉凯斯
+	"吉伽拉格", -- 杰盖拉格
+	"谢尔格拉斯", -- 谢尔格拉
+	"勒丹亚", -- 瑞达尼亚
+}
+
+for _, filename in ipairs(filenames) do
+	local i = 0
+	for line in io.lines(filename) do
+		i = i + 1
+		for _, forbid in ipairs(forbids) do
+			if line:find(forbid, 1, true) then
+				print(filename .. "(" .. i .. "): " .. forbid)
+			end
+		end
+	end
+end
+
+print "done!"
