d978e3287ca2c585055f5987f834226bcca15a41
dwing <dwing4g@gmail.com>
2025-09-28 20:34:33 +0800

    some fixes for tes3cn_*; add tag in SSQN mod for location translation

--- mods/SkyrimStyleQuest/Scripts/SSQN/player.lua
+++ mods/SkyrimStyleQuest/Scripts/SSQN/player.lua
@@ -339,7 +339,7 @@ time.runRepeatedly(function()
 				locations[name:lower()] = cells
 				if settings:get("showDiscover") then
 					I.SSQN.showBanner{
-						text=settings:get("discoverUpper") and name:upper() or name,
+						text="#{sCell="..name.."}", -- settings:get("discoverUpper") and name:upper() or name,
 						header=l10n("text_discover")
 					}
 				end
